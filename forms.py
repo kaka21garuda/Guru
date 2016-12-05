980ff6f37c5f8f60c67a97ccb5c8107baa19e116
Buka Cakrawala <kaka10garuda@gmail.com>
2016-12-04 19:23:24 -0800

    added login forms and view

--- forms.py
+++ forms.py
@@ -42,3 +42,7 @@ class RegisterForm(Form):
         'Confirm Password',
         validators = [DataRequired()]
     )
+
+class LoginForm(Form):
+    email = StringField('Email', validators = [DataRequired(), Email()])
+    password = PasswordField('Password', validators = [DataRequired()])
