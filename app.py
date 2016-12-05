d29506146250ca9e86b84ac34a47d8e402b35a36
Buka Cakrawala <kaka10garuda@gmail.com>
2016-12-04 18:19:56 -0800

    added registration forms and macro view

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import (Flask, g, render_template, flash, redirect, url_for)
+from flask_login import LoginManager
+
+import forms
+import models
+
+DEBUG = True
+PORT = 8000
+HOST = '0.0.0.0'
+
+app = Flask(__name__)
+app.secret_key = "secretkey"
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(userid):
+    try:
+        return models.User.get(models.User.id == userid)
+    except models.DoesNotExist:
+        return None
+
+@app.before_request
+def before_request():
+    # connect to the database before each request.
+    g.db = models.DATABASE
+    g.db.connect()
+
+@app.after_request
+def after_request(response):
+# close the databse connection after each request
+    g.db.close()
+    return response
+
+@app.route('/register', methods = ('GET', 'POST'))
+def register():
+    form = forms.RegisterForm()
+    if form.validate_on_submit():
+        flash("Welcome to Guru!", "success")
+        models.User.create_user(
+            username = form.username.data,
+            email = form.email.data,
+            password = form.password.data
+        )
+        return redirect(url_for('index'))
+    return render_template('register.html', form = form)
+
+@app.route('/')
+def index():
+    return 'Hey!'
+
+
+if __name__ == '__main__':
+    models.initialize()
+    try:
+        models.User.create_user(
+            username = "bukacak",
+            email = "bukacak@gmail.com",
+            password = "password",
+            admin = True
+        )
+    except ValueError:
+        pass
+    app.run(debug = DEBUG, port = PORT, host = HOST)
