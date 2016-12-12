5bfbc8606a53033491fe6600bbb3d072f82f7562
Buka Cakrawala <kaka10garuda@gmail.com>
2016-12-11 16:55:35 -0800

    enabled to send and received message

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+
+app = Flask(__name__, template_folder="views")
+app.config['SECRET_KEY'] = 'secret'
+socketio = SocketIO(app)
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@socketio.on('message')
+def handle_message(msg):
+    print("Message: " + msg)
+    send(msg, broadcast=True)
+
+
+if __name__ == '__main__':
+    socketio.run(app)
